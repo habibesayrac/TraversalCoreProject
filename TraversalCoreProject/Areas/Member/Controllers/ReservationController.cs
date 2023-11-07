26e2a5997940de3a9b7ca83ba463e9c89d2cae40
habibesayrac <habibesayrac@outlook.com>
2023-11-07 13:15:52 +0300

    reservation table created

--- TraversalCoreProject/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProject/Areas/Member/Controllers/ReservationController.cs
@@ -1,11 +1,17 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace TraversalCoreProject.Areas.Member.Controllers
 {
     [Area("Member")]
     public class ReservationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager reservationManager= new ReservationManager(new EfReservationDal());
+
         public IActionResult MyCurrentReservation() {
             return View(); 
         } 
@@ -16,12 +22,25 @@ namespace TraversalCoreProject.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationID.ToString()
+
+                                           }).ToList();
+            ViewBag.v = values;
             return View();
         }
+
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            return View();
+            p.AppUserID = 4;
+
+            reservationManager.TAdd(p);
+
+            return RedirectToAction("MyCurrentReservation");
         }
     }
 }
