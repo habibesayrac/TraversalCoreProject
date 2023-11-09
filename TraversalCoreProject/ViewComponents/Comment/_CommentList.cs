4005623e062a3813fb4a49361deebb489603f0bd
habibesayrac <habibesayrac@outlook.com>
2023-11-09 22:35:19 +0300

    guide null problem solved

--- TraversalCoreProject/ViewComponents/Comment/_CommentList.cs
+++ TraversalCoreProject/ViewComponents/Comment/_CommentList.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.ViewComponents.Comment
 {
     public class _CommentList :ViewComponent
     {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
 
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(int id)
         {
+            //var values = commentManager.TGetDestinationById(id);
             return View();
         }
     }
