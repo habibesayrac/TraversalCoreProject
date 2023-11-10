76602fa639de4cbf94cefabd3b7e87161089ade5
habibesayrac <habibesayrac@outlook.com>
2023-11-10 09:22:51 +0300

    AdminLayout added

--- TraversalCoreProject/Program.cs
+++ TraversalCoreProject/Program.cs
@@ -54,4 +54,12 @@ app.UseEndpoints(endpoints =>
     );
 });
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.Run();
