b127cd4a07054a14621bd85d78e113a438f53931
habibesayrac <habibesayrac@outlook.com>
2023-11-13 20:31:48 +0300

    admindashboard updated

---
+++ TraversalCoreProject/ViewComponents/AdminDashboard/_Cards2Statistic.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.AdminDashboard
+{
+    public class _Cards2Statistic:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/AdminDashboard/_DestinationStatistic2.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.AdminDashboard
+{
+    public class _DestinationStatistic2 : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+
+        {
+            return View();
+        }
+    }
+}
