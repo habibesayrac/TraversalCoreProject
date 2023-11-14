1dd8f0299e03ba262444633d6366ccd42200c9a1
habibesayrac <habibesayrac@outlook.com>
2023-11-14 13:23:38 +0300

    commentpage created

---
+++ TraversalCoreProject/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+        public IActionResult Index()
+        {
+            var values = _commentService.TGetListCommentWithDestination();
+            return View(values);
+        } 
+        public IActionResult DeleteComment(int id)
+        {
+            var values = _commentService.TGetById(id);
+            _commentService.TDelete(values);
+            return RedirectToAction("Index");
+
+          
+        }
+    }
+}
