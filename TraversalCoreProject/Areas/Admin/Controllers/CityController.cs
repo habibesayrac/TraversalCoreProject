5ac46ab7063791584b1d5f3336fd89664daabce6
habibesayrac <habibesayrac@outlook.com>
2023-11-20 21:51:16 +0300

    ajax_destination page added

--- TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TraversalCoreProject.Models;
 
@@ -7,6 +9,11 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
     [Area("Admin")]
     public class CityController : Controller
     {
+        private readonly IDestinationService _destinationService;
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,31 +21,48 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
 
         public IActionResult CityList()
         {
-            var jsonCity = JsonConvert.SerializeObject(cities);
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
             return Json(jsonCity);
         }
 
-        public static List<CityClass> cities = new List<CityClass>
-        {
-            new CityClass()
-            {
-                CityID= 1,
-                CityName ="Üsküp",
-                CityCountry ="Makedonya"
-            },
-            new CityClass()
-            {
-                CityID= 2,
-                CityName ="Roma",
-                CityCountry ="İtalya"
-            },
-            new CityClass()
-            {
-                CityID= 3,
-                CityName ="Londra",
-                CityCountry ="İngiltere"
-            }
+        //public static List<CityClass> cities = new List<CityClass>
+        //{
+        //    new CityClass()
+        //    {
+        //        CityID= 1,
+        //        CityName ="Üsküp",
+        //        CityCountry ="Makedonya"
+        //    },
+        //    new CityClass()
+        //    {
+        //        CityID= 2,
+        //        CityName ="Roma",
+        //        CityCountry ="İtalya"
+        //    },
+        //    new CityClass()
+        //    {
+        //        CityID= 3,
+        //        CityName ="Londra",
+        //        CityCountry ="İngiltere"
+        //    }
 
-        };
+        //};
+
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination destination)
+        {
+            //destination.Status = true;
+            _destinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination);
+            return Json(values);
+        }
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            
+            var values= _destinationService.TGetById(id);
+            var jsonValues = JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+        }
     }
 }
