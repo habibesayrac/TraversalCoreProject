f88dc3244d65bbe2042dd336fb6ed15486dd4bce
habibesayrac <habibesayrac@outlook.com>
2023-11-26 15:21:57 +0300

    bookinghotelApi added

---
+++ TraversalCoreProject/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class BookingHotelSearchController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?units=metric&locale=en-gb&checkin_date=2024-05-19&dest_type=city&order_by=price&filter_by_currency=EUR&adults_number=2&room_number=1&dest_id=-1456928&checkout_date=2024-05-20&include_adjacency=true&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&page_number=5&children_ages=5%2C0&children_number=2"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "e32d0223f5msh90bdc26a183e3c0p11dca3jsn0190e2427676" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel>(body);
+                return View(values.results);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCityDestID()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetCityDestID(string p)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?name={p}&locale=en-gb"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "e32d0223f5msh90bdc26a183e3c0p11dca3jsn0190e2427676" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+        
+                return View();
+            }
+        }
+    }
+
+
+}
---
+++ TraversalCoreProject/Areas/Admin/Models/BookingHotelSearchViewModel.cs
@@ -0,0 +1,88 @@
+﻿namespace TraversalCoreProject.Areas.Admin.Models
+{
+    public class BookingHotelSearchViewModel
+    {
+
+            public object count { get; set; }
+            public object mapPageFields { get; set; }
+            public Result[] results { get; set; }
+        
+        public class Result
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public int mainPhotoId { get; set; }
+            public string photoMainUrl { get; set; }
+            public string[] photoUrls { get; set; }
+            public int position { get; set; }
+            public int rankingPosition { get; set; }
+            public string countryCode { get; set; }
+            public float latitude { get; set; }
+            public float longitude { get; set; }
+            public Pricebreakdown priceBreakdown { get; set; }
+            public string currency { get; set; }
+            public Checkin checkin { get; set; }
+            public Checkout checkout { get; set; }
+            public string checkoutDate { get; set; }
+            public string checkinDate { get; set; }
+            public float reviewScore { get; set; }
+            public string reviewScoreWord { get; set; }
+            public int reviewCount { get; set; }
+            public int qualityClass { get; set; }
+            public object isFirstPage { get; set; }
+            public int accuratePropertyClass { get; set; }
+            public int propertyClass { get; set; }
+            public int ufi { get; set; }
+            public string wishlistName { get; set; }
+            public int optOutFromGalleryChanges { get; set; }
+        }
+
+        public class Pricebreakdown
+        {
+            public Benefitbadge[] benefitBadges { get; set; }
+            public object[] taxExceptions { get; set; }
+            public Grossprice grossPrice { get; set; }
+            public Excludedprice excludedPrice { get; set; }
+            public Strikethroughprice strikethroughPrice { get; set; }
+        }
+
+        public class Grossprice
+        {
+            public float value { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Excludedprice
+        {
+            public float value { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Strikethroughprice
+        {
+            public float value { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Benefitbadge
+        {
+            public string text { get; set; }
+            public string explanation { get; set; }
+            public string variant { get; set; }
+            public string identifier { get; set; }
+        }
+
+        public class Checkin
+        {
+            public string untilTime { get; set; }
+            public string fromTime { get; set; }
+        }
+
+        public class Checkout
+        {
+            public string untilTime { get; set; }
+            public string fromTime { get; set; }
+        }
+
+    }
+}
