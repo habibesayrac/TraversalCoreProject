a0021ea872bd8ef40938f4c47580d2ee82eef124
habibesayrac <habibesayrac@outlook.com>
2023-12-06 12:47:13 +0300

    SignalR added

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,12 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using SignalRApi.DAL;
+using SignalRApi.Hubs;
+using SignalRApi.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<Context>(opt =>
-opt.UseNpgsql(connectionString));
+//builder.Services.AddEntityFrameworkNpgsql().AddDbContext<Context>(opt =>
+//opt.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<VisitorService>();
+builder.Services.AddSignalR();
+builder.Services.AddDbContext<Context>();
+//builder.Services.AddHttpClient();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,3 +35,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+//app.MapHub<VisitorHub>("/chatHub");
