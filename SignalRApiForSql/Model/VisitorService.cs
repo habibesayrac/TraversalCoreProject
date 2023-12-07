51962307fd583a8e0cce45a17bd2acd151f74859
habibesayrac <habibesayrac@outlook.com>
2023-12-07 20:30:46 +0300

    SignalRApiForSql updated

--- SignalRApiForSql/Model/VisitorService.cs
+++ SignalRApiForSql/Model/VisitorService.cs
@@ -23,14 +23,15 @@ namespace SignalRApiForSql.Model
         {
             await _context.Visitors.AddAsync(visitor);
             await _context.SaveChangesAsync();
-            await _hubContext.Clients.All.SendAsync("CallVisitorList", "bbb");
+            await _hubContext.Clients.All.SendAsync("ReceiveVisitorList", GetVisitorChartList());
         }
         public List<VisitorChart> GetVisitorChartList()
         {
             List<VisitorChart> visitorCharts = new List<VisitorChart>();
             using (var command = _context.Database.GetDbConnection().CreateCommand())
             {
-                command.CommandText = "query sorgu";
+                command.CommandText = "select tarih,[1],[2],[3],[4],[5],[6] from (select[City],CityVisitCount,Cast([VisitDate] as Date) as tarih from Visitors)as visitTable Pivot " +
+                    "(Sum(CityVisitCount) For City in([1],[2],[3],[4],[5],[6])) as pivottable order by tarih asc";
                 command.CommandType = System.Data.CommandType.Text;
                 _context.Database.OpenConnection();
                 using (var reader = command.ExecuteReader())
@@ -39,10 +40,16 @@ namespace SignalRApiForSql.Model
                     {
                         VisitorChart visitorChart = new();
                         visitorChart.VisitDate = reader.GetDateTime(0).ToShortDateString();
-                        Enumerable.Range(1, 5).ToList().ForEach(x =>
+                        Enumerable.Range(1, 6).ToList().ForEach(x =>
                         {
-                            visitorChart.Counts.Add(reader.GetInt32(x));
-
+                            if (DBNull.Value.Equals(reader[x]))
+                            {
+                                visitorChart.Counts.Add(0);
+                            }
+                            else
+                            {
+                                visitorChart.Counts.Add(reader.GetInt32(x));
+                            }
                         });
                         visitorCharts.Add(visitorChart);
                     }
--- SignalRApiForSql/Program.cs
+++ SignalRApiForSql/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
 
