8163e8b0f53c4c1ee1f475a6a4b0d526d79f27d7
habibesayrac <habibesayrac@outlook.com>
2023-12-11 12:58:16 +0300

    Language and search added

--- TraversalCoreProject/Areas/Member/Controllers/DestinationController.cs
+++ TraversalCoreProject/Areas/Member/Controllers/DestinationController.cs
@@ -5,8 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Areas.Member.Controllers
 {
-    [AllowAnonymous]
     [Area("Member")]
+    [Route("Member/[controller]/[action]")]
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
@@ -14,6 +14,16 @@ namespace TraversalCoreProject.Areas.Member.Controllers
         {
             var values = destinationManager.TGetList();
             return View(values);
+        }
+        public IActionResult GetCitiesSearchByName(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            var values = from x in destinationManager.TGetList() select x;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                values = values.Where(y => y.City.Contains(searchString));
+            }
+            return View(values.ToList());
         }
     }
 }
